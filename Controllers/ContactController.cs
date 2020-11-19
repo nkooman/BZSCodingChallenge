bc9a6669c2845bf6025a516626a8e298ab5ed72c
Nick Kooman <nickjkooman@gmail.com>
2020-11-19 13:17:30 -0500

    Initial commit

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using BZSCodingChallenge.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BZSCodingChallenge.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IWebHostEnvironment env;
+
+        public ContactController(IWebHostEnvironment env) => this.env = env;
+
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Index(ContactFormModel formModel)
+        {
+            if (ModelState.IsValid == true)
+            {
+                System.IO.File.WriteAllText(env.ContentRootPath + "/responses.json", JsonConvert.SerializeObject(formModel));
+
+                // Quick way to clear the form for this challenge.
+                return Redirect("/");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BZSCodingChallenge.Models
+{
+    public class ContactFormModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
